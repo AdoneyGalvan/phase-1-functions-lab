40a5a2451ad140355b53e5fe2fff4ae26b6a1790
Aron Galvan <arongalvan03@gmail.com>
2022-05-22 10:18:03 -0700

    commit tree 2356810473476c29efef61fa02e2cc44c1245ee9

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+const distanceFromHqInBlocks = (destination) => {
+    return Math.abs(destination - 42);
+}
+
+const distanceFromHqInFeet = (destination) => {
+    return distanceFromHqInBlocks(destination)*264;
+}
+
+ const distanceTravelledInFeet = (start, destination) => {
+    return Math.abs(destination - start)*264;
+ }
+
+ const calculatesFarePrice = (start, destination) => {
+    let distance = Math.abs(destination - start)*264;;
+    
+    if (distance <= 400)
+    {
+        return 0;
+    }
+    if (distance > 400 && distance < 2000)
+    {
+        return (distance - 400)*0.02;
+    }
+    if (distance >= 2000 && distance < 2500)
+    {
+        return 25;
+    }
+    if (distance >= 2500)
+    {
+        return "cannot travel that far";
+    }
+
+ }
